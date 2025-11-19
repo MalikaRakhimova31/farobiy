ee3de427f8a57572cb6cc3358daee97f1d04e524
MalikaRakhimova31 <malikarakhimova31@gmail.com>
2025-11-19 23:36:36 +0500

    feat: twitter card added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
 
-  modules: ["@nuxt/image", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  modules: ["@nuxt/image", "@nuxtjs/color-mode", "@nuxtjs/i18n", "nuxt-aos"],
 
   css: ["assets/css/main.css"],
   vite: {
@@ -38,14 +38,37 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      title: "Farobiy School - International Education",
+      title: "Farobiy International School",
       meta: [
         {
           name: "description",
           content:
-            "Farobiy School - providing world-class international education with modern teaching methods",
+            "Farobiy International School zamonaviy ta'lim va yuqori sifatli bilimni ta'minlaydi. Bizning maqsadimiz – har bir o'quvchini kelajakda muvaffaqiyatli yetakchi qilish.",
+        },
+
+        { property: "og:title", content: "Farobiy International School" },
+        {
+          property: "og:description",
+          content:
+            "Farobiy International School zamonaviy ta'lim va yuqori sifatli bilimni ta'minlaydi. Bizning maqsadimiz – har bir o'quvchini kelajakda muvaffaqiyatli yetakchi qilish.",
+        },
+        {
+          property: "og:image",
+          content: "https://farobiy-red.vercel.app/hero.webp",
+        },
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: "https://farobiy-red.vercel.app/" },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "Farobiy International School" },
+        {
+          name: "twitter:description",
+          content:
+            "Farobiy International School zamonaviy ta'lim va yuqori sifatli bilimni ta'minlaydi. Bizning maqsadimiz – har bir o'quvchini kelajakda muvaffaqiyatli yetakchi qilish.",
+        },
+        {
+          name: "twitter:image",
+          content: "https://farobiy-red.vercel.app/hero.webp",
         },
       ],
     },
